beb2afc1b716bc70e3dc84591240c81999949f1a
Shota Togawa <shotatogawa@Shotas-MacBook-Pro.local>
2026-06-11 13:18:58 +0900

    feat: connect mongodb but it's temporary way

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import bookRouter from "./routers/books.js";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 3000;
 
+// TODO: We need to decide how to handle this.
+mongoose.connect("mongodb://localhost:27017/bookkeep");
+
+app.use(express.json());
 app.use(express.static("client"));
 app.use("/api", bookRouter);
 
