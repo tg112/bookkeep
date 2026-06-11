97eb0096b57731cac5fef3c8e7976c56f1502a1e
Shota Togawa <shotatogawa@Shotas-MacBook-Pro.local>
2026-06-11 13:02:16 +0900

    feat: add a router in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import bookRouter from "./routers/books.js";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("frontend"));
+app.use(express.static("client"));
+app.use("/api", bookRouter);
 
 app.listen(PORT, () => {
   console.log("listening 3000...");
