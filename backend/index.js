bad28efa340663bbcce5a77a60048b38e6c85dab
Shota Togawa <shotatogawa@Shotas-MacBook-Pro.local>
2026-06-15 20:28:45 +0900

    chore: fix cors error

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ import booksRouter from "./routers/books.js";
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://tg112.github.io"],
+  }),
+);
 app.use(express.json());
 app.use("/api", booksRouter);
 
